864807d0cdee85e3febb9c914a61de67f7f9352e
shreyas66s <shreyas1665@gmail.com>
2023-11-02 17:19:17 +0530

    First commit

---
+++ gfg.py
@@ -0,0 +1,13 @@
+"""
+This is a simple script that calculates the sum of two numbers.
+"""
+
+# Define two constants with meaningful names in UPPER_CASE
+NUM1 = 3
+NUM2 = 2
+
+# Calculate the sum of the two constants and store it in a new constant
+RESULT = NUM1 + NUM2
+
+# Print a descriptive message along with the result
+print(f"The sum of {NUM1} and {NUM2} is: {RESULT}")
